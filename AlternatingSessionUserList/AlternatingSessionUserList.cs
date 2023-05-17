d1e9cf5d2147dc34b1987ff6980836cb2e3d086f
Arne Kiesewetter <banane9@live.de>
2023-05-17 21:25:00 +0200

    Add project files.

---
+++ AlternatingSessionUserList/AlternatingSessionUserList.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using BaseX;
+using CodeX;
+using FrooxEngine;
+using FrooxEngine.LogiX;
+using FrooxEngine.LogiX.Data;
+using FrooxEngine.LogiX.ProgramFlow;
+using FrooxEngine.UIX;
+using HarmonyLib;
+using NeosModLoader;
+
+namespace AlternatingSessionUserList
+{
+    public class AlternatingSessionUserList : NeosMod
+    {
+        public static ModConfiguration Config;
+
+        [AutoRegisterConfigKey]
+        private static readonly ModConfigurationKey<color> FirstRowColor = new("FirstRowColor", "Background color of the first row in the Session user lists.", () => new color(0, .85f));
+
+        [AutoRegisterConfigKey]
+        private static readonly ModConfigurationKey<color> SecondRowColor = new("SecondRowColor", "Background color of the second row in the Session user lists.", () => new color(1, .15f));
+
+        public override string Author => "Banane9";
+        public override string Link => "https://github.com/Banane9/NeosAlternatingSessionUserList";
+        public override string Name => "AlternatingSessionUserList";
+        public override string Version => "1.0.0";
+
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new($"{Author}.{Name}");
+            Config = GetConfiguration();
+            Config.Save(true);
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(SessionControlDialog))]
+        private static class SessionControlDialogPatches
+        {
+            private static IEnumerable<Slot> GetUserRows(SessionControlDialog.Tab tab, Slot uiRoot)
+            {
+                return tab switch
+                {
+                    SessionControlDialog.Tab.Users => uiRoot.GetComponentsInChildren<SessionUserController>().Select(controller => controller.Slot),
+                    SessionControlDialog.Tab.Permissions => uiRoot.GetComponentsInChildren<SessionPermissionController>().Select(controller => controller.Slot),
+                    _ => Array.Empty<Slot>(),
+                };
+            }
+
+            [HarmonyPostfix]
+            [HarmonyPatch("OnCommonUpdate")]
+            private static void OnCommonUpdatePostfix(Sync<SessionControlDialog.Tab> ___ActiveTab, SyncRef<Slot> ____uiContentRoot)
+            {
+                var first = true;
+
+                foreach (var userRow in GetUserRows(___ActiveTab, ____uiContentRoot))
+                {
+                    if (userRow.GetComponent<LayoutElement>() is LayoutElement layout && layout.MinHeight == SessionPermissionController.HEIGHT)
+                    {
+                        var childOffset = new float2(0, 4);
+                        layout.MinHeight.Value = SessionPermissionController.HEIGHT + 8;
+
+                        var children = userRow.Children.Select(child => child.GetComponent<RectTransform>()).Where(rect => rect != null).ToArray();
+                        children[0].OffsetMin.Value += childOffset.yx;
+                        children[^1].OffsetMax.Value -= childOffset.yx;
+
+                        foreach (var childRect in children)
+                        {
+                            childRect.OffsetMax.Value -= childOffset;
+                            childRect.OffsetMin.Value += childOffset;
+                        }
+                    }
+
+                    var image = userRow.AttachComponent<Image>();
+                    image.Tint.Value = Config.GetValue(first ? FirstRowColor : SecondRowColor);
+
+                    first = !first;
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+Alternating Session User-List
+=============================
+
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that adds alternating colors to the rows in the Session tab's Users and Permissions lists.  
+The colors can be customized in the mod settings.
+
+## Installation
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+2. Place [DynVarSpaceTree.dll](https://github.com/Banane9/NeosDynVarSpaceTree/releases/latest/download/DynVarSpaceTree.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+3. Start the game. If you want to verify that the mod is working you can check your Neos logs.
+
+## Example
+
+![Users tab with alternating row colors](https://media.discordapp.net/attachments/1108351167991402506/1108457201569374218/image.png?width=1248&height=676)
+
+![Permissions tab with alternating row colors](https://media.discordapp.net/attachments/1108351167991402506/1108457202223689779/image.png?width=1248&height=676)
